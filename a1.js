be0b51f90e2356a6a417df38045cdc5b97dc80f4
xiaoxiaohuang <hxj886600@outlook.com>
2017-10-29 11:03:36 +0800

    first commit

---
+++ a1.js
@@ -0,0 +1,28 @@
+
+var nodemailer = require('nodemailer');  
+var transporter = nodemailer.createTransport({  
+  service: 'qq',  
+  auth: {  
+    user: '1240040337@qq.com',  
+    pass: 'kldjfhmefstmhdbb' //授权码,通过QQ获取  
+  
+  }  
+  });  
+  var mailOptions = {  
+    from: '1240040337@qq.com', // 发送者  
+    to: '845307004@qq.com', // 接受者,可以同时发送多个,以逗号隔开  
+    subject: 'nodemailer2.5.0邮件发送', // 标题  
+    //text: 'Hello world', // 文本  
+    html: `<h2>nodemailer基本使用:</h2><h3>  
+    <a href="http://blog.csdn.net/zzwwjjdj1/article/details/51878392">  
+    http://blog.csdn.net/zzwwjjdj1/article/details/51878392</a></h3>`   
+  };  
+  
+  transporter.sendMail(mailOptions, function (err, info) {  
+    if (err) {  
+      console.log(err);  
+      return;  
+    }  
+  
+    console.log('发送成功');  
+  }); 
